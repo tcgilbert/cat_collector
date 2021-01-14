009f7955f21d6ba51675170eb02b766273823ada
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-14 15:49:12 -0500

    add update and delete urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Cat
 
 
@@ -9,6 +11,7 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+############### CATS ################
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', { 'cats': cats })
@@ -16,3 +19,21 @@ def cats_index(request):
 def cats_show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/show.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
